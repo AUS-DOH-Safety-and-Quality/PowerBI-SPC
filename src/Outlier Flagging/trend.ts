df244798c91c9847751b471d069387e427646377
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2022-11-08 17:48:49 +0200

    Highlight all points in a given pattern

--- src/Outlier Flagging/trend.ts
+++ src/Outlier Flagging/trend.ts
@@ -8,9 +8,19 @@ function trend(val: number[], n: number): boolean[] {
   let lagged_sign_sum: number[] = lagged_sign.map((d, i) => {
     return d3.sum(lagged_sign.slice(Math.max(0, i - (n - 2)), i + 1));
   })
-  return lagged_sign_sum.map(d => {
+  let trend_detected: boolean[] = lagged_sign_sum.map(d => {
     return math.abs(d) >= (n - 1);
   })
+
+  for (let i: number = 0; i < trend_detected.length; i++) {
+    if (trend_detected[i] === true) {
+      for (let j: number = (i - 1); j >= (i - (n - 1)); j--) {
+        trend_detected[j] = true;
+      }
+    }
+  }
+
+  return trend_detected;
 }
 
 export default trend
--- src/Outlier Flagging/two_in_three.ts
+++ src/Outlier Flagging/two_in_three.ts
@@ -8,9 +8,19 @@ function two_in_three(val: number[], ll95: number[], ul95: number[]): boolean[] 
   let lagged_sign_sum: number[] = outside95.map((d, i) => {
     return d3.sum(outside95.slice(Math.max(0, i - 2), i + 1));
   })
-  return lagged_sign_sum.map(d => {
+  let two_in_three_detected: boolean[] = lagged_sign_sum.map(d => {
     return d >= 2;
   })
+
+  for (let i: number = 0; i < two_in_three_detected.length; i++) {
+    if (two_in_three_detected[i] === true) {
+      for (let j: number = (i - 1); j >= (i - 2); j--) {
+        two_in_three_detected[j] = true;
+      }
+    }
+  }
+
+  return two_in_three_detected;
 }
 
 export default two_in_three
