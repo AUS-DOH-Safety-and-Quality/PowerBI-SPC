704f0537aa7ea161d7f3b8c2018d1debfb9d091b
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-01-21 16:14:40 +0200

    Update functions to broadcast

--- src/Functions/stringToDMY.ts
+++ src/Functions/stringToDMY.ts
@@ -1,14 +1,15 @@
 import dateFormat from "../Classes/dateFormat";
+import broadcast_binary from "./BinaryFunctions"
 
 //Function to handle string-to-date conversions with JS's weird conventions
-function stringToDMY(input_datestrings: string[], date_format: dateFormat): string[] {
-  return input_datestrings.map(dateString => {
-    let step1: Date = new Date(dateString);
-    let step2: Date = new Date(step1.getTime() + Math.abs(step1.getTimezoneOffset() * 60000));
+function stringToDMY_impl(input_datestring: string, date_format: dateFormat): string {
+  let step1: Date = new Date(input_datestring);
+  let step2: Date = new Date(step1.getTime() + Math.abs(step1.getTimezoneOffset() * 60000));
 
-    return step2.toLocaleDateString(date_format.locale, date_format.options)
-                .replace(/(\/|(\s|,\s))/gi, date_format.delimiter);
-  })
+  return step2.toLocaleDateString(date_format.locale, date_format.options)
+              .replace(/(\/|(\s|,\s))/gi, date_format.delimiter);
 }
+
+const stringToDMY = broadcast_binary(stringToDMY_impl)
 
 export default stringToDMY;
--- src/Functions/truncate.ts
+++ src/Functions/truncate.ts
@@ -1,22 +1,16 @@
-function truncate<T extends number | number[]>(
-  val: T, limits: {lower?: number, upper?: number}
-): T {
-  let rtn: T = val;
+import broadcast_binary from "./BinaryFunctions"
+
+function truncate_impl(val: number, limits: {lower?: number, upper?: number}): number {
+  let rtn: number = val;
   if (limits.lower || limits.lower == 0) {
-    if (Array.isArray(val)) {
-      rtn = val.map(d => d < limits.lower ? limits.lower : d) as T;
-    } else if (typeof val === "number") {
-      rtn = (rtn < limits.lower ? limits.lower : rtn) as T;
-    }
+    rtn = (rtn < limits.lower ? limits.lower : rtn)
   }
   if (limits.upper) {
-    if (Array.isArray(rtn)) {
-      rtn = rtn.map(d => d > limits.upper ? limits.upper : d) as T;
-    } else if (typeof rtn === "number") {
-      rtn = (rtn > limits.upper ? limits.upper : rtn) as T;
-    }
+    rtn = (rtn > limits.upper ? limits.upper : rtn);
   }
   return rtn;
 }
+
+const truncate = broadcast_binary(truncate_impl)
 
 export default truncate;
