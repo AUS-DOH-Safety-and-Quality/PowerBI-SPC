d4ef7a998d4fa233858f8b102a3133c415f081b4
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-01-22 17:14:02 +0200

    Simplify viewmodel constructor

--- src/Functions/buildTooltip.ts
+++ src/Functions/buildTooltip.ts
@@ -1,22 +1,8 @@
 import powerbi from "powerbi-visuals-api";
 import VisualTooltipDataItem = powerbi.extensibility.VisualTooltipDataItem;
-
-
-type tooltipArgs = {
-  date: string,
-  value: number,
-  numerator?: number,
-  denominator?: number,
-  target: number,
-  limits: { ll99: number, ul99: number},
-  chart_type: string,
-  multiplier: number,
-  prop_labels: boolean,
-  astpoint: string,
-  trend: string,
-  shift: string,
-  two_in_three: string
-}
+import controlLimits from "../Classes/controlLimits";
+import dataObject from "../Classes/dataObject";
+import settingsObject from "../Classes/settingsObject";
 
 let valueNames: Record<string, string> = {
   "i": "Observation",
@@ -33,66 +19,85 @@ let valueNames: Record<string, string> = {
   "up": "Rate"
 }
 
-function buildTooltip(args: tooltipArgs, sig_figs: number): VisualTooltipDataItem[] {
+function buildTooltip(index: number, calculatedLimits: controlLimits,
+                      inputData: dataObject, inputSettings: settingsObject): VisualTooltipDataItem[] {
+
+  let date: string = calculatedLimits.keys[index].label;
+  let value: number = calculatedLimits.values[index];
+  let numerator: number = calculatedLimits.numerators ? calculatedLimits.numerators[index] : null;
+  let denominator: number = calculatedLimits.denominators ? calculatedLimits.denominators[index] : null;
+  let target: number = calculatedLimits.targets[index];
+  let limits = {
+      ll99: calculatedLimits.ll99 ? calculatedLimits.ll99[index] : null,
+      ul99: calculatedLimits.ll99 ? calculatedLimits.ul99[index] : null
+    };
+  let chart_type: string = inputData.chart_type;
+  let prop_labels: boolean = inputData.percentLabels;
+  let astpoint: string = calculatedLimits.astpoint[index];
+  let trend: string = calculatedLimits.trend[index];
+  let shift: string = calculatedLimits.shift[index];
+  let two_in_three: string = calculatedLimits.two_in_three[index];
+
+  let sig_figs: number = inputSettings.spc.sig_figs.value;
   let tooltip: VisualTooltipDataItem[] = new Array<VisualTooltipDataItem>();
   tooltip.push({
     displayName: "Date",
-    value: args.date
+    value: date
   });
   tooltip.push({
-    displayName: valueNames[args.chart_type],
-    value: args.prop_labels
-      ? (args.value * 100).toFixed(sig_figs) + "%"
-      : args.value.toFixed(sig_figs)
+    displayName: valueNames[chart_type],
+    value: prop_labels
+      ? (value * 100).toFixed(sig_figs) + "%"
+      : value.toFixed(sig_figs)
   })
-  if(args.numerator || !(args.numerator === null || args.numerator === undefined)) {
+  if(numerator || !(numerator === null || numerator === undefined)) {
     tooltip.push({
       displayName: "Numerator",
-      value: (args.numerator).toFixed(sig_figs)
+      value: (numerator).toFixed(sig_figs)
     })
   }
-  if(args.denominator || !(args.denominator === null || args.denominator === undefined)) {
+  if(denominator || !(denominator === null || denominator === undefined)) {
     tooltip.push({
       displayName: "Denominator",
-      value: (args.denominator).toFixed(sig_figs)
+      value: (denominator).toFixed(sig_figs)
     })
   }
-  if (args.chart_type !== "run") {
+  if (chart_type !== "run") {
     tooltip.push({
       displayName: "Upper 99% Limit",
-      value: args.prop_labels
-        ? (args.limits.ul99 * 100).toFixed(sig_figs) + "%"
-        : args.limits.ul99.toFixed(sig_figs)
+      value: prop_labels
+        ? (limits.ul99 * 100).toFixed(sig_figs) + "%"
+        : limits.ul99.toFixed(sig_figs)
     })
   }
   tooltip.push({
     displayName: "Centerline",
-    value: args.prop_labels
-      ? (args.target * 100).toFixed(sig_figs) + "%"
-      : args.target.toFixed(sig_figs)
+    value: prop_labels
+      ? (target * 100).toFixed(sig_figs) + "%"
+      : target.toFixed(sig_figs)
   })
-  if (args.chart_type !== "run") {
+  if (chart_type !== "run") {
     tooltip.push({
       displayName: "Lower 99% Limit",
-      value: args.prop_labels
-        ? (args.limits.ll99 * 100).toFixed(sig_figs) + "%"
-        : args.limits.ll99.toFixed(sig_figs)
+      value: prop_labels
+        ? (limits.ll99 * 100).toFixed(sig_figs) + "%"
+        : limits.ll99.toFixed(sig_figs)
     })
   }
 
-  if (args.astpoint !== "none" || args.trend !== "none" ||
-      args.shift !== "none" || args.two_in_three !== "none") {
+  if (astpoint !== "none" || trend !== "none" ||
+      shift !== "none" || two_in_three !== "none") {
     let patterns: string[] = new Array<string>();
-    if (args.astpoint !== "none") {
+    if (astpoint !== "none") {
       patterns.push("Astronomical Point")
     }
-    if (args.trend !== "none") {
+    if (trend !== "none") {
       patterns.push("Trend")
     }
-    if (args.shift !== "none") {
+    if (shift !== "none") {
       patterns.push("Shift")
     }
-    if (args.two_in_three !== "none") {
+    if (two_in_three !== "none") {
       patterns.push("Two-in-Three")
     }
     tooltip.push({
