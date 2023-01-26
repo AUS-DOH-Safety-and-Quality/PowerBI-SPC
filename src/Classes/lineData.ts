35da9053098be7b3d1b2877132194bd65de8ebb7
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-01-26 14:01:14 +0200

    Simplify lineData spec

--- src/Classes/lineData.ts
+++ src/Classes/lineData.ts
@@ -2,12 +2,6 @@ class lineData {
   x: number;
   line_value: number;
   group: string;
-
-  constructor() {
-    this.x = null;
-    this.line_value = null;
-    this.group = null;
-  }
 };
 
 export default lineData;
