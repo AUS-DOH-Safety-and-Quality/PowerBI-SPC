12077777d0729860c8832ed2e553164a03e9f92e
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-07-28 21:46:11 +0300

    Tidy & bump version

--- src/Functions/diff.ts
+++ src/Functions/diff.ts
@@ -1,7 +1,5 @@
 export default function diff(x: number[]): number[] {
-  const consec_diff: number[] = new Array<number>(x.length - 1);
-  for (let i = 1; i < x.length;  i++) {
-    consec_diff[(i-1)] = (x[i] - x[(i-1)]);
-  }
-  return [<number>null].concat(consec_diff);
+  return x.map((d, idx, arr) =>
+    (idx > 0) ? d - arr[idx - 1] : null
+  );
 }
