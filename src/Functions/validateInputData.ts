7a19cdc8018f582ebc9962fce0ceba045befa169
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-09-17 19:13:38 +0300

    Fix incorrect warning message on data removal

--- src/Functions/validateInputData.ts
+++ src/Functions/validateInputData.ts
@@ -13,33 +13,33 @@ export default function validateInputData(keys: string[], numerators: number[], 
   }
   const numeratorNonNegativeRequired: string[] = ["p", "pp", "u", "up", "s", "c", "g", "t"];
   const status: string[] = rep("", keys.length);
-  keys.forEach((d, idx) => status[idx] = (status[idx] === "" && d != null) ? "" : "Date missing");
+  keys.forEach((d, idx) => status[idx] = status[idx] === "" ? ((d != null) ? "" : "Date missing") : status[idx]);
   if (!status.some(d => d == "")) {
     throw(new validationErrorClass("All dates/IDs are missing or null!"))
   }
 
-  numerators.forEach((d, idx) => status[idx] = (status[idx] === "" && d != null) ? "" : "Numerator missing");
+  numerators.forEach((d, idx) => status[idx] = status[idx] === "" ? ((d != null) ? "" : "Numerator missing") : status[idx]);
   if (!status.some(d => d == "")) {
     throw(new validationErrorClass("All numerators are missing or null!"))
   }
   if (numeratorNonNegativeRequired.includes(data_type)) {
-    numerators.forEach((d, idx) => status[idx] = (status[idx] === "" && d >= 0) ? "" : "Numerator negative");
+    numerators.forEach((d, idx) => status[idx] = status[idx] === "" ? ((d >= 0) ? "" : "Numerator negative") : status[idx]);
     if (!status.some(d => d == "")) {
       throw(new validationErrorClass("All numerators are negative!"))
     }
   }
 
   if (denominatorRequired.includes(data_type)) {
-    denominators.forEach((d, idx) => status[idx] = (status[idx] === "" && d != null) ? "" : "Denominator missing");
+    denominators.forEach((d, idx) => status[idx] = status[idx] === "" ? ((d != null) ? "" : "Denominator missing") : status[idx]);
     if (!status.some(d => d == "")) {
       throw(new validationErrorClass("All denominators missing or null!"))
     }
-    denominators.forEach((d, idx) => status[idx] = (status[idx] === "" && d >= 0) ? "" : "Denominator negative");
+    denominators.forEach((d, idx) => status[idx] = status[idx] === "" ? ((d >= 0) ? "" : "Denominator negative") : status[idx]);
     if (!status.some(d => d == "")) {
       throw(new validationErrorClass("All denominators are negative!"))
     }
     if (denominatorConstraintRequired.includes(data_type)) {
-      denominators.forEach((d, idx) => status[idx] = (status[idx] === "" && d >= numerators[idx]) ? "" : "Denominator < numerator");
+      denominators.forEach((d, idx) => status[idx] = status[idx] === "" ? ((d >= numerators[idx]) ? "" : "Denominator < numerator") : status[idx]);
       if (!status.some(d => d == "")) {
         throw(new validationErrorClass("All denominators are smaller than numerators!"))
       }
@@ -47,11 +47,11 @@ export default function validateInputData(keys: string[], numerators: number[], 
   }
 
   if (data_type === "xbar") {
-    xbar_sds.forEach((d, idx) => status[idx] = (status[idx] === "" && d != null) ? "" : "SD missing");
+    xbar_sds.forEach((d, idx) => status[idx] = status[idx] === "" ? ((d != null) ? "" : "SD missing") : status[idx]);
     if (!status.some(d => d == "")) {
       throw(new validationErrorClass("All SDs missing or null!"))
     }
-    xbar_sds.forEach((d, idx) => status[idx] = (status[idx] === "" && d >=0) ? "" : "SD negative");
+    xbar_sds.forEach((d, idx) => status[idx] = status[idx] === "" ? ((d >=0) ? "" : "SD negative") : status[idx]);
     if (!status.some(d => d == "")) {
       throw(new validationErrorClass("All SDs are negative!"))
     }
