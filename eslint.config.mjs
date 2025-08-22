6f63440cb0ae7446cfe55f8b40abdf0eef9c67c4
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-08-22 13:21:10 +0800

    Update lint inclusion rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,16 @@ import powerbiVisualsConfigs from "eslint-plugin-powerbi-visuals";
 export default [
     powerbiVisualsConfigs.configs.recommended,
     {
-        ignores: ["node_modules/**", "dist/**", ".vscode/**", ".tmp/**", "test/**", "rollup.config.js"]
+        ignores: [
+            "node_modules/**",
+            "dist/**",
+            ".vscode/**",
+            ".tmp/**",
+            "test/**",
+            "coverage/**",
+            "rollup.config.js",
+            "karma.conf.ts",
+            "test.webpack.config.js"
+        ]
     },
 ];
