6a8364df48a8ef90769d9cd8ef7280cd425eaf0c
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-10-31 09:46:43 +0800

    Simplify rendering for value labels

--- src/D3 Plotting Functions/drawValueLabels.ts
+++ src/D3 Plotting Functions/drawValueLabels.ts
@@ -3,113 +3,46 @@ import type { svgBaseType, Visual } from "../visual";
 import type { plotData } from "../Classes";
 import { isValidNumber } from "../Functions";
 
-const labelFormatting = function(selection: d3.Selection<d3.BaseType, plotData, d3.BaseType, unknown>, visualObj: Visual) {
-  // -90 degrees for vertically above, 90 degrees for vertically below
-  const allData = selection.data();
-  const initialLabelXY: {x: number, y: number, theta: number, line_offset: number, marker_offset: number}[] = allData.map(d => {
-    const label_direction_mult: number = d.label.aesthetics.label_position === "top" ? -1 : 1;
-    const plotHeight: number = visualObj.viewModel.svgHeight;
-    const xAxisHeight: number = plotHeight - visualObj.plotProperties.yAxis.start_padding;
-    const label_position: string = d.label.aesthetics.label_position;
-    let y_offset: number = d.label.aesthetics.label_y_offset;
-    const label_initial: number = label_position === "top" ? (0 + y_offset) : (xAxisHeight - y_offset);
-    const y: number = visualObj.plotProperties.yScale(d.value);
-    let side_length: number = label_position === "top" ? (y - label_initial) : (label_initial - y);
-    const x_val = visualObj.plotProperties.xScale(d.x);
-    const y_val = visualObj.plotProperties.yScale(d.value);
+function getLabelAttributes(d: plotData, visualObj: Visual): {x: number, y: number, theta: number, line_offset: number, marker_offset: number} {
+  const label_direction_mult: number = d.label.aesthetics.label_position === "top" ? -1 : 1;
+  const plotHeight: number = visualObj.viewModel.svgHeight;
+  const xAxisHeight: number = plotHeight - visualObj.plotProperties.yAxis.start_padding;
+  const label_position: string = d.label.aesthetics.label_position;
+  let y_offset: number = d.label.aesthetics.label_y_offset;
+  const label_initial: number = label_position === "top" ? (0 + y_offset) : (xAxisHeight - y_offset);
+  const y: number = visualObj.plotProperties.yScale(d.value);
+  let side_length: number = label_position === "top" ? (y - label_initial) : (label_initial - y);
+  const x_val = visualObj.plotProperties.xScale(d.x);
+  const y_val = visualObj.plotProperties.yScale(d.value);
 
-    const theta: number = d.label.angle ?? (d.label.aesthetics.label_angle_offset + label_direction_mult * 90);
-    side_length = d.label.distance ?? (Math.min(side_length, d.label.aesthetics.label_line_max_length));
+  const theta: number = d.label.angle ?? (d.label.aesthetics.label_angle_offset + label_direction_mult * 90);
+  side_length = d.label.distance ?? (Math.min(side_length, d.label.aesthetics.label_line_max_length));
 
-    let line_offset: number = d.label.aesthetics.label_line_offset;
-    line_offset = label_position === "top" ? line_offset : -(line_offset + d.label.aesthetics.label_size / 2);
+  let line_offset: number = d.label.aesthetics.label_line_offset;
+  line_offset = label_position === "top" ? line_offset : -(line_offset + d.label.aesthetics.label_size / 2);
 
-    let marker_offset: number = d.label.aesthetics.label_marker_offset + d.label.aesthetics.label_size / 2;
-    marker_offset = label_position === "top" ? -marker_offset : marker_offset;
+  let marker_offset: number = d.label.aesthetics.label_marker_offset + d.label.aesthetics.label_size / 2;
+  marker_offset = label_position === "top" ? -marker_offset : marker_offset;
 
-    const newX: number = x_val + side_length * Math.cos(theta * Math.PI / 180);
-    const newY: number = y_val + side_length * Math.sin(theta * Math.PI / 180);
+  const newX: number = x_val + side_length * Math.cos(theta * Math.PI / 180);
+  const newY: number = y_val + side_length * Math.sin(theta * Math.PI / 180);
 
-    if (!isValidNumber(newX) || !isValidNumber(newY)) {
-      return {
-        x: 0,
-        y: 0,
-        theta: 0,
-        line_offset: 0,
-        marker_offset: 0
-      };
-    }
+  if (!isValidNumber(newX) || !isValidNumber(newY)) {
+    return {
+      x: 0,
+      y: 0,
+      theta: 0,
+      line_offset: 0,
+      marker_offset: 0
+    };
+  }
 
-    return {x: newX,
-            y: newY,
-            theta: theta,
-            line_offset: line_offset,
-            marker_offset: marker_offset
-          };
-
-  })
-
-  selection.select("text")
-            .text(d => d.label.text_value)
-            .attr("x", (_, i) => initialLabelXY[i].x)
-            .attr("y", (_, i) => initialLabelXY[i].y)
-            .style("text-anchor", "middle")
-            .style("font-size", d => `${d.label.aesthetics.label_size}px`)
-            .style("font-family", d => d.label.aesthetics.label_font)
-            .style("fill", d => d.label.aesthetics.label_colour);
-
-  selection.select("line")
-            .attr("x1", (_, i) => initialLabelXY[i].x)
-            .attr("y1", (_, i) => {
-              if (initialLabelXY[i].x === 0 && initialLabelXY[i].y === 0) {
-                return 0;
-              }
-              return initialLabelXY[i].y + initialLabelXY[i].line_offset;
-            })
-            .attr("x2", (d, i) => {
-              if (initialLabelXY[i].x === 0 && initialLabelXY[i].y === 0) {
-                return 0;
-              }
-              const marker_offset: number = initialLabelXY[i].marker_offset;
-              const angle: number = initialLabelXY[i].theta - (d.label.aesthetics.label_position === "top" ? 180 : 0);
-              return visualObj.plotProperties.xScale(d.x) + marker_offset * Math.cos(angle * Math.PI / 180);
-            })
-            .attr("y2", (d, i) => {
-              if (initialLabelXY[i].x === 0 && initialLabelXY[i].y === 0) {
-                return 0;
-              }
-              const marker_offset: number = initialLabelXY[i].marker_offset;
-              const angle: number = initialLabelXY[i].theta -(d.label.aesthetics.label_position === "top" ? 180 : 0);
-              return visualObj.plotProperties.yScale(d.value) + marker_offset * Math.sin(angle * Math.PI / 180);
-            })
-            .style("stroke", visualObj.viewModel.inputSettings.settings.labels.label_line_colour)
-            .style("stroke-width", d => (d.label.text_value ?? "") === "" ? 0 : visualObj.viewModel.inputSettings.settings.labels.label_line_width)
-            .style("stroke-dasharray", visualObj.viewModel.inputSettings.settings.labels.label_line_type);
-
-  selection.select("path")
-            .attr("d", d => {
-              const show_marker: boolean = d.label.aesthetics.label_marker_show && (d.label.text_value ?? "") !== "";
-              const marker_size: number = show_marker ? Math.pow(d.label.aesthetics.label_marker_size, 2) : 0;
-              return d3.symbol().type(d3.symbolTriangle).size(marker_size)()
-            })
-            .attr("transform", (d, i) => {
-              const show_marker: boolean = d.label.aesthetics.label_marker_show && (d.label.text_value ?? "") !== "";
-              if ((initialLabelXY[i].x === 0 && initialLabelXY[i].y === 0) || !show_marker) {
-                return "translate(0, 0) rotate(0)";
-              }
-              const marker_offset: number = initialLabelXY[i].marker_offset;
-              //const label_position: string = d.label.aesthetics.label_position;
-              const x: number = visualObj.plotProperties.xScale(d.x);
-              const y: number = visualObj.plotProperties.yScale(d.value);
-              const angle: number = initialLabelXY[i].theta - (d.label.aesthetics.label_position === "top" ? 180 : 0);
-              const x_offset: number = marker_offset * Math.cos(angle * Math.PI / 180);
-              const y_offset: number = marker_offset * Math.sin(angle * Math.PI / 180);
-
-              return `translate(${x + x_offset}, ${y + y_offset}) rotate(${angle + (d.label.aesthetics.label_position === "top" ? 90 : 270)})`;
-            })
-            .style("fill", d => d.label.aesthetics.label_marker_colour)
-            .style("stroke", d => d.label.aesthetics.label_marker_outline_colour)
-  return selection;
+  return {x: newX,
+          y: newY,
+          theta: theta,
+          line_offset: line_offset,
+          marker_offset: marker_offset
+        };
 }
 
 export default function drawLabels(selection: svgBaseType, visualObj: Visual) {
@@ -161,15 +94,59 @@ export default function drawLabels(selection: svgBaseType, visualObj: Visual) {
             .data(visualObj.viewModel.plotPoints)
             .join("g")
             .classed("text-group-inner", true)
-            .each(function(_) {
+            .each(function(d: plotData) {
               const textGroup = d3.select(this);
+              if ((d.label.text_value ?? "") === "") {
+                textGroup.remove();
+                return;
+              }
+
               textGroup.selectAll("*").remove();
-              textGroup.append("text");
-              textGroup.append("line");
-              textGroup.append("path");
+              const textElement = textGroup.append("text");
+              const lineElement = textGroup.append("line");
+              const pathElement = textGroup.append("path");
+
+              const { x, y, line_offset, marker_offset, theta } = getLabelAttributes(d, visualObj);
+              const invalidXY: boolean = x === 0 && y === 0;
+              if (invalidXY) {
+                textGroup.remove();
+                return;
+              }
+              const angle: number = theta - (d.label.aesthetics.label_position === "top" ? 180 : 0);
+              const angleToRadians: number = angle * Math.PI / 180;
+
+              textElement
+                .attr("x", x)
+                .attr("y", y)
+                .text(d.label.text_value)
+                .style("text-anchor", "middle")
+                .style("font-size", `${d.label.aesthetics.label_size}px`)
+                .style("font-family", d.label.aesthetics.label_font)
+                .style("fill", d.label.aesthetics.label_colour);
+
+              const markerSize: number = Math.pow(d.label.aesthetics.label_marker_size, 2);
+              const markerX = visualObj.plotProperties.xScale(d.x) + marker_offset * Math.cos(angleToRadians);
+              const markerY = visualObj.plotProperties.yScale(d.value) + marker_offset * Math.sin(angleToRadians);
+
+              lineElement
+                .attr("x1", x)
+                .attr("y1", y + line_offset)
+                .attr("x2", markerX)
+                .attr("y2", markerY)
+                .style("stroke", visualObj.viewModel.inputSettings.settings.labels.label_line_colour)
+                .style("stroke-width", visualObj.viewModel.inputSettings.settings.labels.label_line_width)
+                .style("stroke-dasharray", visualObj.viewModel.inputSettings.settings.labels.label_line_type);
+
+              const markerRotation = angle + (d.label.aesthetics.label_position === "top" ? 90 : 270);
+
+              pathElement
+                .attr("d", d3.symbol().type(d3.symbolTriangle).size(markerSize)())
+                .attr("transform", `translate(${markerX}, ${markerY}) rotate(${markerRotation})`)
+                .style("fill", d.label.aesthetics.label_marker_colour)
+                .style("stroke", d.label.aesthetics.label_marker_outline_colour);
+
               if (!visualObj.viewModel.headless) {
                 textGroup.call(dragFun);
               }
-            })
-            .call(labelFormatting, visualObj);
+            });
 }
