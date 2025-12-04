4dd5b6cbfe7c7038d6b4007c8e0cfd5db5e74eb3
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-12-04 13:42:02 +0800

    Remove linkedom from frontend bundle, minify

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,20 @@ import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: 'src/frontend.ts',
   output: {
-    format: 'umd',
+    format: 'iife',
     name: 'spc',
     file: './.tmp/build/PBISPC.js'
   },
   plugins: [
     typescript({ tsconfig: 'tsconfig.json' }),
     json(),
-    nodeResolve(),
-    commonjs()
+    nodeResolve({ browser: true }),
+    commonjs(),
+    terser()
   ]
 };
--- src/frontend.ts
+++ src/frontend.ts
@@ -1,12 +1,4 @@
 export * as d3 from "./D3 Plotting Functions/D3 Modules";
 
 export { defaultSettings } from './settings';
-export * from './Classes';
-export * from './Functions';
-export * from './D3 Plotting Functions';
-export * from './Limit Calculations'
-export * from './Outlier Flagging'
 export { Visual } from './visual';
-
-// Headless use needs a dummy DOM implementation
-export { parseHTML } from "linkedom"
