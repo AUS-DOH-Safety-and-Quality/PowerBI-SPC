12d144f74b888ec7d922b893877753786f088218
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2026-01-06 20:20:46 +0800

    Fix rendering rebaselines

--- src/D3 Plotting Functions/drawLines.ts
+++ src/D3 Plotting Functions/drawLines.ts
@@ -1,6 +1,6 @@
 import * as d3 from "./D3 Modules";
 import type { defaultSettingsType, lineData } from "../Classes";
-import { getAesthetic, isNullOrUndefined, between } from "../Functions";
+import { getAesthetic, isNullOrUndefined/*, between*/ } from "../Functions";
 import type { svgBaseType, Visual } from "../visual";
 
 export default function drawLines(selection: svgBaseType, visualObj: Visual) {
@@ -12,34 +12,37 @@ export default function drawLines(selection: svgBaseType, visualObj: Visual) {
       .classed("linegroup", true)
       .each(function(currLineData: [string, lineData[]]) {
         const currLine: string = currLineData[0];
-        const validLineData: lineData[] = currLineData[1].filter((d: lineData) => {
-          const ylower: number = visualObj.plotProperties.yAxis.lower;
-          const yupper: number = visualObj.plotProperties.yAxis.upper;
-          const xlower: number = visualObj.plotProperties.xAxis.lower;
-          const xupper: number = visualObj.plotProperties.xAxis.upper;
-          return !isNullOrUndefined(d.line_value)
-                      && between(d.line_value, ylower, yupper)
-                      && between(d.x, xlower, xupper)
-        })
+        const n: number = currLineData[1].length;
+        let validStatus: boolean[] = new Array<boolean>(n);
+        let xValues: number[] = new Array<number>(n);
+        let yValues: number[] = new Array<number>(n);
+
+        for (let i = 0; i < n; i++) {
+          const currPoint: lineData = currLineData[1][i];
+          xValues[i] = visualObj.plotProperties.xScale(currPoint.x);
+          yValues[i] = visualObj.plotProperties.yScale(currPoint.line_value);
+          validStatus[i] = !isNullOrUndefined(currPoint.line_value);
+        }
+
         d3.select(this)
           .selectAll("line")
-          .data(validLineData.slice(1) as lineData[])
+          .data(currLineData[1].slice(1) as lineData[])
           .join("line")
-          .attr("x1", (_, idx: number) => visualObj.plotProperties.xScale(validLineData[idx].x))
-          .attr("y1", (_, idx: number) => visualObj.plotProperties.yScale(validLineData[idx].line_value))
-          .attr("x2", (d: lineData) => visualObj.plotProperties.xScale(d.x))
-          .attr("y2", (d: lineData) => visualObj.plotProperties.yScale(d.line_value))
+          .attr("x1", (_, idx: number) => validStatus[idx] ? xValues[idx] : xValues[idx + 1])
+          .attr("y1", (_, idx: number) => validStatus[idx] ? yValues[idx] : yValues[idx + 1])
+          .attr("x2", (_, idx: number) => validStatus[idx + 1] ? xValues[idx + 1] : xValues[idx])
+          .attr("y2", (_, idx: number) => validStatus[idx + 1] ? yValues[idx + 1] : yValues[idx])
           .attr("fill", "none")
           .attr("stroke", (d: lineData) => {
             return visualObj.viewModel.colourPalette.isHighContrast
                     ? visualObj.viewModel.colourPalette.foregroundColour
-                    : getAesthetic(currLine, "lines", "colour", {lines: d.aesthetics} as defaultSettingsType)
+                    : getAesthetic(currLine, "lines", "colour", { lines: d.aesthetics } as defaultSettingsType)
           })
-          .attr("stroke-width", (d: lineData) => getAesthetic(currLine, "lines", "width", {lines: d.aesthetics} as defaultSettingsType))
-          .attr("stroke-dasharray", (d: lineData) => getAesthetic(currLine, "lines", "type", {lines: d.aesthetics} as defaultSettingsType))
+          .attr("stroke-width", (d: lineData) => getAesthetic(currLine, "lines", "width", { lines: d.aesthetics } as defaultSettingsType))
+          .attr("stroke-dasharray", (d: lineData) => getAesthetic(currLine, "lines", "type", { lines: d.aesthetics } as defaultSettingsType))
           .attr("stroke-dashoffset", (_, idx: number) => {
-              const prev_x: number = visualObj.plotProperties.xScale(validLineData[0].x);
-              const curr_x: number = visualObj.plotProperties.xScale(validLineData[idx].x);
+              const prev_x: number = visualObj.plotProperties.xScale(currLineData[1][0].x);
+              const curr_x: number = visualObj.plotProperties.xScale(currLineData[1][idx].x);
               return curr_x - prev_x
           })
       })
