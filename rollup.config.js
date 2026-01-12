eb517b46e6a2ab277457996ac7b026572a454d97
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2026-01-12 08:21:55 +0800

    Don't minify bundle

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from "@rollup/plugin-json";
-import terser from "@rollup/plugin-terser";
 
 export default {
   input: 'src/frontend.ts',
@@ -16,7 +15,6 @@ export default {
     typescript({ tsconfig: 'tsconfig.json' }),
     json(),
     nodeResolve({ browser: true }),
-    commonjs(),
-    terser()
+    commonjs()
   ]
 };
